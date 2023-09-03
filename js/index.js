85b2efd3b18ccb10bbe40478184cd4d01155ecb2
Prantxesko <xsaylor@hotmail.com>
2023-09-03 10:10:08 +0200

    Se eliminan comentándolos en index.js los filtros del navbar

--- js/index.js
+++ js/index.js
@@ -48,33 +48,33 @@ filterBtn.addEventListener("click", () => {
 
 const navbar = document.querySelector(".collapse.navbar-collapse");
 const labelsMap = DataHandler.labels;
-for (let label in labelsMap) {
-  const name = labelsMap[label].name || "";
-  const filterEl = document.createElement("div");
-  filterEl.className = "form-check form-switch";
-  filterEl.innerHTML = `
-  <input
-    class="form-check-input"
-    type="checkbox"
-    role="switch"
-    id=${label}
-  />
-  <label class="form-check-label" for=${label}
-    >${name}</label
-  >
-  `;
-  const check = filterEl.querySelector("input");
-  check.addEventListener("click", ({ target: { id, checked } }) => {
-    if (checked) {
-      DataHandler.addFilter(id);
-    } else {
-      DataHandler.removeFilter(id);
-    }
-    document.querySelector(".accordion")?.remove();
-    showAccordion(container, DataHandler.data);
-  });
-  navbar.append(filterEl);
-}
+// for (let label in labelsMap) {
+//   const name = labelsMap[label].name || "";
+//   const filterEl = document.createElement("div");
+//   filterEl.className = "form-check form-switch";
+//   filterEl.innerHTML = `
+//   <input
+//     class="form-check-input"
+//     type="checkbox"
+//     role="switch"
+//     id=${label}
+//   />
+//   <label class="form-check-label" for=${label}
+//     >${name}</label
+//   >
+//   `;
+//   const check = filterEl.querySelector("input");
+//   check.addEventListener("click", ({ target: { id, checked } }) => {
+//     if (checked) {
+//       DataHandler.addFilter(id);
+//     } else {
+//       DataHandler.removeFilter(id);
+//     }
+//     document.querySelector(".accordion")?.remove();
+//     showAccordion(container, DataHandler.data);
+//   });
+//   navbar.append(filterEl);
+// }
 
 showAccordion(container, DataHandler.data);
 
